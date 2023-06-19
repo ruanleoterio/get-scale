aae8187d45bfaea7a3a8800682081c7344038cdd
ruanleoterio <ruanleoterio@gmail.com>
2023-06-19 14:32:28 -0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import useSound from 'use-sound';
+import Tonica from './tonica/tonica';
+import cMajor from "./sounds/major/c-major/cmajor.mp3"
+import Subdom from './subdom/Subdom';
+
 
 function App() {
+
+  const [play] = useSound(cMajor, {
+    sprite: {
+      c: [1, 1200],
+      dm: [1710, 1200],
+      em: [3420, 1200],
+      f: [5140, 1200],
+      g7: [6850, 1200],
+      am: [8570, 1200],
+      b: [10285, 1200],
+    },
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Get scale</h1>
+     <Tonica/>
+     <Subdom/>
+
+      <button aria-label="Mi minor"
+        onMouseDown={() => play({ id: 'em' })}>Em</button>
+
+      <button aria-label="Sol major 7"
+        onMouseDown={() => play({ id: 'g7' })}>G7</button>
+      <button aria-label="Sí major 5b7"
+        onMouseDown={() => play({ id: 'b' })}>B7b5</button>
     </div>
   );
 }
